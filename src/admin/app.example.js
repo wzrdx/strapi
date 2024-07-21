52866fc6f2efdb974b8b05159ad64f052a2f50d3
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-07-21 23:59:52 +0300

    Test webhook

--- src/admin/app.example.js
+++ src/admin/app.example.js
@@ -1,32 +1,5 @@
 const config = {
-  locales: [
-    // 'ar',
-    // 'fr',
-    // 'cs',
-    // 'de',
-    // 'dk',
-    // 'es',
-    // 'he',
-    // 'id',
-    // 'it',
-    // 'ja',
-    // 'ko',
-    // 'ms',
-    // 'nl',
-    // 'no',
-    // 'pl',
-    // 'pt-BR',
-    // 'pt',
-    // 'ru',
-    // 'sk',
-    // 'sv',
-    // 'th',
-    // 'tr',
-    // 'uk',
-    // 'vi',
-    // 'zh-Hans',
-    // 'zh',
-  ],
+  locales: [],
 };
 
 const bootstrap = (app) => {

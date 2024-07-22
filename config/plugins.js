612cffe660eb9fc53978e1ea838e2c385b75b6b5
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-07-22 13:12:17 +0300

    Log AWS_ACCESS_KEY_ID

--- config/plugins.js
+++ config/plugins.js
@@ -1,26 +1,30 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        s3Options: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
-          region: env("AWS_REGION"),
-          params: {
-            Bucket: env("AWS_BUCKET_NAME"),
+module.exports = ({ env }) => {
+  console.log("AWS_ACCESS_KEY_ID", env("AWS_ACCESS_KEY_ID"));
+
+  return {
+    upload: {
+      config: {
+        provider: "aws-s3",
+        providerOptions: {
+          s3Options: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+            region: env("AWS_REGION"),
+            params: {
+              Bucket: env("AWS_BUCKET_NAME"),
+            },
+          },
+        },
+        // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
+        actionOptions: {
+          upload: {
+            ACL: null,
+          },
+          uploadStream: {
+            ACL: null,
           },
         },
       },
-      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
-      actionOptions: {
-        upload: {
-          ACL: null,
-        },
-        uploadStream: {
-          ACL: null,
-        },
-      },
     },
-  },
-});
+  };
+};

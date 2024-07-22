cd1e9edc13aada0dfd808663ff9545d7b11f368f
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-07-22 11:08:10 +0300

    Add image to Restaurant schema

--- config/database.js
+++ config/database.js
@@ -11,3 +11,21 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+
+// const path = require("path");
+
+// module.exports = ({ env }) => {
+//   return {
+//     connection: {
+//       client: "sqlite",
+//       connection: {
+//         filename: path.join(
+//           __dirname,
+//           "..",
+//           env("DATABASE_FILENAME", ".tmp/data.db")
+//         ),
+//       },
+//       acquireConnectionTimeout: 60000,
+//     },
+//   };
+// };

d816e0826842a099eb64ea148434e0b8ac47680d
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-07-22 21:07:24 +0300

    Add URL to server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: "https://strapi.wzrdx.xyz",
   app: {
     keys: env.array("APP_KEYS"),
   },
